63d6cfbb5e0bf9103ce5b4d875d7a8530618640b
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-15 12:34:17 +0530

    addemp

---
+++ src/app/empadd/empadd.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-empadd',
+  templateUrl: './empadd.component.html',
+  styleUrls: ['./empadd.component.css']
+})
+export class EmpaddComponent {
+  empcode=""
+  empname=""
+  designation=""
+  salary=""
+  username=""
+  password=""
+constructor(private api:ApiService){}
+
+  readValue=()=>
+  {
+    let data:any={"empcode":this.empcode,"empname":this.empname,"designation":this.designation,
+    "salary":this.salary,"username":this.username,"password":this.password}
+    console.log(data)
+    this.api.addEmployee(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if (response.status=="success") {
+          alert("employ added successfully")
+          this.empcode=""
+          this.empname=""
+          this.designation=""
+          this.salary=""
+          this.username=""
+          this.password=""
+
+        } else {
+          alert("something wrong")
+        }
+      }
+    )
+  }
+}
+
+
